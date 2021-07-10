27467589f166fdb1d9c2ba6c34d49bd9074ca534
zooootech <yzmisng@gmail.com>
2021-07-10 18:15:50 +0900

    Update b_1.py

--- b_1.py
+++ b_1.py
@@ -4,18 +4,17 @@
 # -------------------------------------------------
 
 # count() メソッドと max() 関数を使用
-
-s = input()
+S = input()
 
 # str.count(sub[, start[, end]])
-# 文字列メソッドである count() メソッドは、 [start, end] の範囲に、部分文字列 sub が重複せず出現する回数を返します。
-na = s.count("a")
-nb = s.count("b")
-nc = s.count("c")
+# 文字列メソッドである count() は、 [start, end] の範囲に、部分文字列 sub が重複せず出現する回数を返します。
+na = S.count("a")
+nb = S.count("b")
+nc = S.count("c")
 
 # max(iterable, *[, key, default])
 # max(arg1, arg2, *args[, key])
-# 組み込み関数である max() 関数は、 iterable の中で最大の要素、または2つ以上の引数の中で最大のものを返します。
+# 組み込み関数である max() は、 iterable の中で最大の要素、または 2 つ以上の引数の中で最大のものを返します。
 mx = max(na, nb, nc)
 
 if mx == na:
@@ -27,13 +26,13 @@ else:
 
 # -------------------------------------------------
 
-s = input()
+S = input()
 
 na = 0
 nb = 0
 nc = 0
 
-for x in s:
+for x in S:
   if x == "a":
     na += 1
   elif x == "b":
