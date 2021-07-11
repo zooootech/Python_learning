86ecc991d1d65518554d15a61cbcda2d8e878028
zooootech <yzmisng@gmail.com>
2021-07-11 22:30:01 +0900

    Update c_1.py

--- c_1.py
+++ c_1.py
@@ -1,22 +1,28 @@
 # 3 番目
 # https://atcoder.jp/contests/past201912-open/tasks/past201912_c
 
-n = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
 # sorted(iterable, *, key=None, reverse=False)
-# 組み込み関数である sorted() 関数は、iterable の要素を並べ替えた新たなリストを返します。
+# 組み込み関数である sorted() は、iterable の要素を並べ替えた新たなリストを返します。
 # 2 つのオプション引数があり、これらはキーワード引数として指定されなければなりません。
 # reverse は真偽値です。 True がセットされた場合、リストの要素は個々の比較が反転したものとして並び替えられます。
-sn = sorted(n, reverse=True)
-print(sn[2])
+sa = sorted(a, reverse=True)
+print(sa[2])
 
 # -------------------------------------------------
 
-n = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
 # sort(*, key=None, reverse=False)
-# リスト型のメソッドである sort() メソッドは、元のリスト自体が書き換えられる破壊的なソートを行います。
+# リスト型のメソッドである sort() は、元のリスト自体が書き換えられる破壊的なソートを行います。
 # sort() が返すのは None なので注意。
 # デフォルトは昇順なので、降順にソートしたい場合は引数 reverse を True とする。
-n.sort(reverse=True)
-print(n[2])
+a.sort(reverse=True)
+print(a[2])
+
+# -------------------------------------------------
+
+a = list(map(int, input().split()))
+a.sort()
+print(a[-3])
