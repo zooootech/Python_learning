55a2dbf215dc62540a1e748882d35e9da5758bca
zooootech <yzmisng@gmail.com>
2021-07-31 16:53:57 +0900

    Update a_8.py

--- a_8.py
+++ a_8.py
@@ -6,10 +6,12 @@ flag = True
 
 if S[3] == "-":
   for i in range(len(S)):
+    # i が 3 のとき処理をスルーする
     if i == 3:
       continue
 
-    if not S[i].isdigit():
+    # str.isdecimal() は文字列が十進数字か判定する
+    if not S[i].isdecimal():
       flag = False
       break
 else:
@@ -19,3 +21,11 @@ if flag:
   print("Yes")
 else:
   print("No")
+
+# -------------------------------------------------
+
+S = input()
+if S[3] == '-' and S.count('-') == 1:
+    print("Yes")
+else:
+    print("No")
